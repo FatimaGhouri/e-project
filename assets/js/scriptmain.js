c0f99244f5f259b2edb3a2966ae75fe6af7197cf
Habiba Saeed <habiba.saeed.ahmed.1@gmail.com>
2023-08-23 18:49:03 +0500

    navbar error resolved

--- assets/js/scriptmain.js
+++ assets/js/scriptmain.js
@@ -1,1 +1,16 @@
+//Navbar scroll effect & navbar menu btn scripting
 
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 30){
+            $('.main-navbar').addClass('sticky');
+        }else{
+            $('.main-navbar').removeClass('sticky');
+        }
+    })
+
+    $('.menu-btn').click(function(){
+        $('.main-navbar .nav-links').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    })
+})
