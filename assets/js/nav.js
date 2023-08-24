368b8d8ffac354e2a04e65a4232b40a592b29276
Fatima Ghouri <fatimaghouri24@gmail.com>
2023-08-25 04:35:44 +0500

    humbrgr menu changes

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -7,6 +7,8 @@ var navbar = `
                 <li class="nav-item underline-hover"><a href="about.html">About</a></li>
                 <li class="nav-item underline-hover"><a href="gallery.html">Gallery</a></li>
                 <li class="nav-item underline-hover"><a href="contact.html">Contacts</a></li>
+                <li class="nav-item underline-hover show-in-mobile "><a href="login.html">Login</a></li>
+                <li class="nav-item show-in-mobile"><button class="reg-btn"><a href="reg-form.html">Registration</a></button></li>
             </ul>
             <ul class="nav-btns">
                 <li class="nav-item underline-hover"><a href="login.html">Login</a></li>
